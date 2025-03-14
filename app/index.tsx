02cbca3a6ab7ce21eb5bb51f5c1d6a89c6887361
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-14 12:43:53 +0530

    added the color code

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex items-center justify-center h-full">
-      <Text className="font-bold">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-bold text-dark-200 text-5xl">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
