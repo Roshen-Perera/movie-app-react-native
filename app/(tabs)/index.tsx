8ed8570c466863c3d250551059b5557c71cae20a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-18 00:59:29 +0530

    add ScrollView and logo image to tab layout for improved presentation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,9 @@ const index = () => {
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
+      <ScrollView className="flex-1 px-5">
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+      </ScrollView>
     </View>
   );
 };
