f29d4e8b06162fd7cf765455cddf507fb8ecdf7e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-17 18:15:32 +0530

    tabs created for navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View className="flex items-center justify-center h-full">
+      <Text className="font-bold text-dark-200 text-5xl">Ubetta</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
