1fcf283e1d799efbc83618bb083fffe9ce0491a4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-18 01:24:07 +0530

    enhance SearchBar component with placeholder and onPress functionality for improved navigation

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,21 +1,31 @@
-import { View, Text, Image, TextInput } from 'react-native'
-import React from 'react'
-import { icons } from '@/constants/icons'
+import { View, Text, Image, TextInput } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
 
-const SearchBar = () => {
-  return (
-    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
-      <Image source={icons.search} className='size-5' resizeMode="contain" tintColor="#ab8bff" />
-      <TextInput
-        onPress={() => console.log('search')}
-        value=""
-        placeholder='Search'
-        placeholderTextColor='#ab8bff'
-        onChange={() => console.log('search')}
-        className='flex-1 ml-2 text-white'
-        />
-    </View>
-  )
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
 }
 
-export default SearchBar
+const SearchBar = ({ placeholder, onPress }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        value=""
+        placeholder={placeholder}
+        placeholderTextColor="#ab8bff"
+        onChange={() => console.log("search")}
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
