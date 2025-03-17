f074f9b9da7c664541db06b3d5e5d37a2cd68766
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-18 01:11:51 +0530

    add SearchBar component to tab layout for enhanced user experience

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 const index = () => {
   return (
@@ -13,6 +14,9 @@ const index = () => {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar />
+        </View>
       </ScrollView>
     </View>
   );
