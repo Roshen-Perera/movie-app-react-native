631ec0e900c151285bff683037b4f3aa9a78463d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-18 00:59:16 +0530

    update tab layout with background image and change primary color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 const index = () => {
   return (
-    <View className="flex items-center justify-center h-full">
-      <Text className="font-bold text-dark-200 text-5xl">Ubetta</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
     </View>
   );
 };
