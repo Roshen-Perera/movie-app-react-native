afad8ab21fc9e1067c2a05c2537c1d6c461c3dc7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-20 13:28:16 +0530

    add movie fetching functionality with loading and error handling and corrected auth token path

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 const index = () => {
   const router = useRouter(); // This is a hook from expo-router that allows you to navigate between screens
-
+  const { 
+    data: movies, 
+    loading: moviesLoading, 
+    error: moviesError
+  } = useFetch(() => fetchMovies({
+      query: "",
+  }));
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -17,12 +25,34 @@ const index = () => {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for movies"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text className="">Error: {moviesError?.message}</Text>
+        ) : (
+          <View>
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for movies"
+            />
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3"> Latest Movies </Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => (
+                  <Text className="text-white text-sm">{item.title}</Text>
+                )}
+
+
+              /> 
+            </>
+          </View>
+        )}
+        <View className="flex-1 mt-5"></View>
       </ScrollView>
     </View>
   );
