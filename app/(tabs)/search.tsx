266d91b4425a1cb3d7d41b7dd64d2780d19bc8e0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-23 14:56:50 +0530

    enhance Search component with movie fetching, loading indicator, error handling, and search functionality

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,13 +1,80 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { images } from '@/constants/images'
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import MovieCart from "@/components/movieCart";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 const Search = () => {
-  return (
-    <View className='flex-1 bg-primary'>
-      <Image source={images.bg} className='absolute w-full z-0' />
-    </View>
-  )
-}
+  const router = useRouter(); // This is a hook from expo-router that allows you to navigate between screens
 
-export default Search
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
+  return (
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCart {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar placeholder="Search movies ..." />
+            </View>
+            {moviesLoading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+            {moviesError && (
+              <Text className="text-red-500 text-center my-3">
+                Error: {moviesError?.message}
+              </Text>
+            )}
+            {!moviesLoading &&
+              !moviesError &&
+              "SEARCH TERM".trim() &&
+              movies?.length > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">SEARCH TERM</Text>
+                </Text>
+              )}
+          </>
+        }
+      />
+    </View>
+  );
+};
+
+export default Search;
